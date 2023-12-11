859e6109483604a8c21c20372566f47ec4e287d2
yuzhantao <yuzhantao@qq.com>
2023-12-12 00:37:45 +0800

    add browser of browser tools

--- cat2bug-platform-ui/src/router/index.js
+++ cat2bug-platform-ui/src/router/index.js
@@ -211,7 +211,7 @@ Router.prototype.replace = function push(location) {
 }
 
 export default new Router({
-  mode: 'history', // 去掉url中的#
+  mode: 'hash', // 去掉url中的#
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
