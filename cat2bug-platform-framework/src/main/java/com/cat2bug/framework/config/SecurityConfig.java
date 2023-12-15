cb449a7c7e6cce980718726904223a80109bfa18
yuzhantao <yuzhantao@qq.com>
2023-12-16 02:08:20 +0800

    update test browser

--- cat2bug-platform-framework/src/main/java/com/cat2bug/framework/config/SecurityConfig.java
+++ cat2bug-platform-framework/src/main/java/com/cat2bug/framework/config/SecurityConfig.java
@@ -111,7 +111,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 // 过滤请求
                 .authorizeRequests()
                 // 对于登录login 注册register 验证码captchaImage 允许匿名访问
-                .antMatchers("/login", "/register", "/captchaImage").permitAll()
+                .antMatchers("/login", "/logout", "/register", "/captchaImage").permitAll()
                 // 静态资源，可匿名访问
                 .antMatchers(HttpMethod.GET, "/", "/*.html", "/**/*.html", "/**/*.css", "/**/*.js", "/profile/**").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
