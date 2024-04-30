bd561009373bf82da4e377412d25722951864873
yuzhantao <yuzhantao@qq.com>
2024-05-01 00:03:42 +0800

    修复手动创建报告异常的bug

--- cat2bug-platform-ui/src/components/Report/ViewReport.vue
+++ cat2bug-platform-ui/src/components/Report/ViewReport.vue
@@ -160,7 +160,7 @@ export default {
     /** 导出Word */
     async exportWord() {
       this.loading = true;
-      let id = "template-data";
+      let id = "report-data";
       // 将canvas转为图片
       let tempImages = this.convertCanvasesToImages(id);
       // 根据样式获取元素并转为图片
