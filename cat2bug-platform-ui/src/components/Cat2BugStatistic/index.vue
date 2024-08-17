ba68215f24ca5882dcea4245fe88b2b9a4a83201
yuzhantao <yuzhantao@qq.com>
2024-08-17 17:08:55 +0800

    修复缺陷统计模版没有初始化悬浮按钮的bug

--- cat2bug-platform-ui/src/components/Cat2BugStatistic/index.vue
+++ cat2bug-platform-ui/src/components/Cat2BugStatistic/index.vue
@@ -134,10 +134,20 @@ export default {
         this.getAllStatisticList();
         break;
     }
+    this.initFloatMenu();
   },
   created() {
   },
+  // 移除滚动条监听
+  destroyed() {
+    this.$floatMenu.windowsDestory();
+  },
   methods: {
+    /** 初始化浮动菜单 */
+    initFloatMenu() {
+      this.$floatMenu.windowsInit(document.querySelector('.main-container'));
+      this.$floatMenu.resetMenus();
+    },
     /** 更新统计面板 */
     updateStatisticPanel: function (){
       let params = {
