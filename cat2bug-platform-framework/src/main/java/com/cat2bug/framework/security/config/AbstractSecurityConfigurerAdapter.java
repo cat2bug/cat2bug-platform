8e9acc83c0b33b92a400ea99fb4c64a53d373bac
yuzhantao <yuzhantao@qq.com>
2024-09-23 18:52:28 +0800

    更新安全配置

--- cat2bug-platform-framework/src/main/java/com/cat2bug/framework/security/config/AbstractSecurityConfigurerAdapter.java
+++ cat2bug-platform-framework/src/main/java/com/cat2bug/framework/security/config/AbstractSecurityConfigurerAdapter.java
@@ -1,5 +1,6 @@
 package com.cat2bug.framework.security.config;
 
+import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 
 /**
@@ -7,7 +8,7 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
  * @CreateTime: 2024-09-23 18:32
  * @Version: 1.0.0
  */
-public abstract class AbstractSecurityConfigurerAdapter extends SecurityConfigurerAdapter {
+public abstract class AbstractSecurityConfigurerAdapter<O, B extends SecurityBuilder<O>> extends SecurityConfigurerAdapter<O, B> {
     /**
      * 获取匹配网址
      * @return
