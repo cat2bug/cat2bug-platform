867b7f30fab0a10f6f585a9202ce7cad60510cf0
yuzhantao <yuzhantao@qq.com>
2024-11-10 19:20:04 +0800

    更新文件配置

--- cat2bug-platform-common/src/main/java/com/cat2bug/common/config/Cat2BugConfig.java
+++ cat2bug-platform-common/src/main/java/com/cat2bug/common/config/Cat2BugConfig.java
@@ -1,7 +1,14 @@
 package com.cat2bug.common.config;
 
+import com.cat2bug.common.utils.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 读取项目相关配置
@@ -109,12 +116,32 @@ public class Cat2BugConfig
         return getProfile() + "/import";
     }
 
+    public static String getImportPath(String path)
+    {
+        List<String> pathList = new ArrayList<>();
+        pathList.add(getProfile());
+        if(StringUtils.isNotBlank(path))
+            pathList.add(path);
+        pathList.add("import");
+        return File.separator + pathList.stream().collect(Collectors.joining(File.separator));
+    }
+
     /**
      * 获取头像上传路径
      */
     public static String getAvatarPath()
     {
         return getProfile() + "/avatar";
+    }
+
+    public static String getAvatarPath(String path)
+    {
+        List<String> pathList = new ArrayList<>();
+        pathList.add(getProfile());
+        if(StringUtils.isNotBlank(path))
+            pathList.add(path);
+        pathList.add("avatar");
+        return File.separator + pathList.stream().collect(Collectors.joining(File.separator));
     }
 
     /**
@@ -125,11 +152,31 @@ public class Cat2BugConfig
         return getProfile() + "/download/";
     }
 
+    public static String getDownloadPath(String path)
+    {
+        List<String> pathList = new ArrayList<>();
+        pathList.add(getProfile());
+        if(StringUtils.isNotBlank(path))
+            pathList.add(path);
+        pathList.add("download");
+        return File.separator + pathList.stream().collect(Collectors.joining(File.separator));
+    }
+
     /**
      * 获取上传路径
      */
     public static String getUploadPath()
     {
         return getProfile() + "/upload";
+    }
+
+    public static String getUploadPath(String path)
+    {
+        List<String> pathList = new ArrayList<>();
+        pathList.add(getProfile());
+        if(StringUtils.isNotBlank(path))
+            pathList.add(path);
+        pathList.add("upload");
+        return File.separator + pathList.stream().collect(Collectors.joining(File.separator));
     }
 }
