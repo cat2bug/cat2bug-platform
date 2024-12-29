051bf716b406d64a8a52aae689e1eafa25456578
yuzhantao <yuzhantao@qq.com>
2024-12-29 22:51:26 +0800

    添加通用项目推送类

---
+++ cat2bug-platform-common/src/main/java/com/cat2bug/common/core/domain/entity/SysProjectPush.java
@@ -0,0 +1,14 @@
+package com.cat2bug.common.core.domain.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: yuzhantao
+ * @CreateTime: 2024-12-29 22:51
+ * @Version: 1.0.0
+ */
+@Data
+public class SysProjectPush {
+    private String version;
+    private String pushKey;
+}
