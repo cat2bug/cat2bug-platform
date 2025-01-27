7a9e5dee756bd10ad3b953877c566e32c029d983
yuzhantao <yuzhantao@qq.com>
2025-01-28 02:05:47 +0800

    C2D普通input组件基础功能

--- cat2bug-platform-ui/src/components/Cat2BugDocument/components/Cat2DocInput/index.vue
+++ cat2bug-platform-ui/src/components/Cat2BugDocument/components/Cat2DocInput/index.vue
@@ -1,44 +1,134 @@
 <template>
   <div>
     <slot name="tools"></slot>
-    <input ref="c2d-input" class="c2d-input" v-model="content.value"
-           @keydown.enter="handleEnter"
-           @keydown.delete="handleDelete"
-    />
+    <textarea ref="c2d-input"
+              :rows="1"
+      class="c2d-input"
+              v-model="inputContent.value"
+      @input="handleInput"
+      @keypress="handleKeyDown"
+      @keydown.up="handleUp"
+      @keydown.left="handleUp"
+      @keydown.down="handleDown"
+      @keydown.right="handleDown"
+      @keydown.delete="handleBack"
+    ></textarea>
   </div>
 </template>
 
 <script>
+const KEY_ENTER = 13;
+const KEY_UP = 38;
+const KEY_DOWN = 40;
+const KEY_LEFT = 37;
+const KEY_RIGHT = 39;
+
 export default {
   name: "Cat2DocInput",
+  model: {
+    prop: 'content',
+    event: 'input'
+  },
   props: {
     content: {
       type: Object,
       default: ()=>{
-        return {
-          value: ''
-        }
+        return {}
       }
+    }
+  },
+  data() {
+    return {
+      inputContent: this.content,
     }
   },
   mounted() {
     this.focus();
   },
+  computed: {
+    textareaContent: function () {
+      return this.inputContent.value?this.inputContent.value.replace('\n',''):null;
+    },
+    c2dInput() {
+      return this.$refs['c2d-input'];
+    }
+  },
   methods: {
+    /** 设置焦点 */
     focus() {
       this.$nextTick(()=>{
-        this.$refs['c2d-input'].focus();
+        this.c2dInput.focus();
       });
     },
-    toData() {
+    /** 获取Json格式内容 */
+    toJson() {
 
     },
-    handleEnter() {
-      this.$emit('enter');
+    /** 在尾部添加内容 */
+    appendValue(value) {
+      this.inputContent.value += value;
+      this.$emit('input', this.inputContent);
     },
-    handleDelete() {
-      if(!this.content.value || this.content.value.length==0) {
-        this.$emit('delete');
+    /** 设置光标选择范围 */
+    selectableRange(start, end) {
+      this.$nextTick(()=>{
+        this.c2dInput.setSelectionRange(start,end);
+      });
+    },
+    /** 处理input */
+    handleInput(event) {
+      this.c2dInput.style.height = 'auto';
+      this.c2dInput.style.height = (this.c2dInput.scrollHeight) + 'px';
+      this.$emit('input', this.inputContent);
+    },
+    /** 处理按键按下 */
+    handleKeyDown(event) {
+      console.log(event.keyCode)
+      switch (event.keyCode) {
+        case KEY_ENTER:
+          return this.handleEnter(event);
+        default:
+          return this.handleString(event);
+      }
+    },
+    /** 处理任意字符 */
+    handleString(event) {
+
+    },
+    /** 处理回车 */
+    handleEnter(event) {
+      const start = this.c2dInput.selectionStart;
+      const end = this.c2dInput.selectionEnd;
+      let frontVal = this.inputContent.value.substr(0,start);
+      let afterVal = this.inputContent.value.substr(end);
+      this.inputContent.value = frontVal;
+      this.handleInput();
+      this.$emit('enter', afterVal);
+      event.preventDefault();
+    },
+    /** 处理回车 */
+    handleUp(event) {
+      const start = this.c2dInput.selectionStart;
+      if(start==0) {
+        this.$emit('up');
+        event.preventDefault();
+      }
+    },
+    /** 处理回车 */
+    handleDown(event) {
+      const end = this.c2dInput.selectionEnd;
+      const len = this.inputContent.value?this.inputContent.value.length:0;
+      if(end==len) {
+        this.$emit('down');
+        event.preventDefault();
+      }
+    },
+    /** 处理退格键 */
+    handleBack(event) {
+      const selectionEnd = this.c2dInput.selectionEnd;
+      if(selectionEnd===0) {
+        this.$emit('delete', this.inputContent.value);
+        event.preventDefault();
       }
     }
   }
@@ -51,5 +141,6 @@ export default {
   font-size: 20px;
   border-width: 0px;
   outline: none;
+  resize:none;
 }
 </style>
--- cat2bug-platform-ui/src/components/Cat2BugDocument/index.vue
+++ cat2bug-platform-ui/src/components/Cat2BugDocument/index.vue
@@ -2,23 +2,35 @@
   <div class="c2d" :style="{width: width}">
     <input class="title" value="title">
 
-    <draggable class="c2d-content" handle=".tools" v-model="componentList">
+    <draggable class="c2d-content" handle=".move" v-model="componentList">
       <transition-group>
-        <component
+        <div
+          class="c2d-block"
           v-for="(c,index) in componentList"
-          :ref="'c2d'+c.code"
           :key="'c2d'+c.code"
-          :is="c.type"
-          @mouseover.native="handleMouseOver($event, c, index)"
-          @mouseleave.native="handleMouseLeave(c, index)"
-          @enter="handleEnter(c, index)"
-          @delete="handleDelete(c, index)">
-          <template #tools>
-            <div v-show="toolsVisible" :ref="'tools'+c.code" class="tools">
-              <el-button size="mini" icon="el-icon-plus" />
-            </div>
-          </template>
-        </component>
+          @mouseover="handleMouseOver($event, c, index)"
+          @mouseleave="handleMouseLeave(c, index)"
+        >
+          <div class="drop-tools">
+            <svg-icon v-show="c.toolsVisible" class="move" icon-class="drag2" />
+          </div>
+          <div class="content">
+            <component
+              :ref="'c2d'+c.code"
+              :is="c.type"
+              :content="c"
+              @up="handleUp(c, index, $event)"
+              @down="handleDown(c, index, $event)"
+              @enter="handleEnter(c, index, $event)"
+              @delete="handleDelete(c, index, $event)">
+<!--              <template #tools>-->
+<!--                <div v-show="c.toolsVisible" class="tools">-->
+<!--                  <el-button size="mini" icon="el-icon-plus" />-->
+<!--                </div>-->
+<!--              </template>-->
+            </component>
+          </div>
+        </div>
       </transition-group>
     </draggable>
   </div>
@@ -44,73 +56,130 @@ export default {
       title: '标题',
       // 组件列表
       componentList: [],
-      // 工具是否显示
-      toolsVisible: false,
+    }
+  },
+  computed: {
+    ele: function () {
+      return function (index) {
+        return this.$refs['c2d'+this.componentList[index].code][0];
+      }
     }
   },
   mounted() {
     this.init();
   },
   methods: {
+    /** 初始化 */
     init() {
-      this.componentList.push(this.makeDefaultComponent())
+      this.componentList.push(this.makeDefaultComponent());
     },
-    makeDefaultComponent() {
+    /** 创建默认input组件 */
+    makeDefaultComponent(value) {
       return {
         code: new Date().getTime(),
-        type: DEFAULT_COMPONENT_NAME
+        type: DEFAULT_COMPONENT_NAME,
+        toolsVisible: false,
+        value: value
       };
     },
-    handleEnter(component, index) {
-      this.componentList.splice(index+1, 0, this.makeDefaultComponent());
-      this.setDragComponentFocus(index+1);
-
-    },
-    handleDelete(component, index) {
-      // 如果是默认Input组件，并且只有一个input，则不删除
-      if(component.type==DEFAULT_COMPONENT_NAME && this.componentList.filter(c=>c.type==DEFAULT_COMPONENT_NAME).length==1) {
-        return;
+    /** 处理向上按钮事件 */
+    handleUp(component, index, event) {
+      if(index>0) {
+        this.setDragComponentFocus(index-1);
       }
-      this.componentList.splice(index, 1);
+    },
+    /** 处理向下按钮事件 */
+    handleDown(component, index, event) {
+      if(index<this.componentList.length - 1) {
+        this.setDragComponentFocus(index+1);
+      }
+    },
+    /** 处理回车事件 */
+    handleEnter(component, index, event) {
+      switch (component.type) {
+        case DEFAULT_COMPONENT_NAME:
+          this.handleInputEnter(component, index, event);
+          break;
+      }
+      this.$nextTick(()=>{
+        this.setDragComponentFocus(index+1);
+      })
+    },
+    /** 处理退格删除事件 */
+    handleDelete(component, index, event) {
+      switch (component.type) {
+        case DEFAULT_COMPONENT_NAME:
+          this.handleInputDelete(component, index, event);
+          break;
+      }
+      // 设置焦点
       const preIndex = Math.max(index-1, 0);
-
       this.setDragComponentFocus(preIndex);
     },
+    /** 处理Input组件的回车事件 */
+    handleInputEnter(component, index, event) {
+      setTimeout(()=>{
+
+      },300)
+      this.componentList.splice(index+1, 0, this.makeDefaultComponent(event));
+      this.$nextTick(()=>{
+        this.ele(index+1).selectableRange(0, 0);
+      })
+    },
+    /** 处理Input组件的退格事件 */
+    handleInputDelete(component, index, value) {
+      // 如果还有文字，将当前组件文字拼接到上一个input组件
+      const preIndex = Math.max(index-1, 0);
+      if(value && this.componentList[preIndex].type === DEFAULT_COMPONENT_NAME) {
+        const start = this.componentList[preIndex].value?this.componentList[preIndex].value.length:0;
+        this.ele(preIndex).appendValue(value);
+        this.ele(preIndex).selectableRange(start,start);
+      }
+      // 如果input大于1个，就删除当前的
+      if(this.componentList.filter(c=>c.type==DEFAULT_COMPONENT_NAME).length>1) {
+        this.componentList.splice(index, 1);
+      }
+    },
+    /** 处理鼠标移入事件 */
     handleMouseOver(event, c, index) {
-      // const top = event.target.offsetTop;
-      this.toolsVisible = true;
+      c.toolsVisible = true;
       // this.$nextTick(()=>{
       //   this.$refs.tools.style.top=top+'px';
       // })
     },
+    /** 处理鼠标移出事件 */
     handleMouseLeave(component, index) {
-      this.toolsVisible = false;
+      component.toolsVisible = false;
     },
+    /** 设置组件获取焦点 */
     setDragComponentFocus(index) {
-      setTimeout(()=>{
-
-      }, 1000)
       this.$nextTick(()=>{
-        this.$refs['c2d'+this.componentList[index].code][0].focus();
+        this.ele(index).focus();
       });
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .c2d {
   padding: 15px calc(100% / 2 - 800px / 2);
-  position: relative;
+  /*position: relative;*/
 }
 .tools {
   position: absolute;
-  left: calc(100% / 2 - 800px / 2 - 50px);
+  padding-right: 10px;
+  right: calc(100% / 2 + 800px / 2);
+  display: inline-flex;
+  flex-direction: row;
+  gap: 5px;
+  align-items: center;
 }
 .title {
   font-size: 40px;
   border-width: 0px;
   outline: none;
+  padding-left: 30px;
   margin-bottom: 15px;
 }
 .c2d-content {
@@ -118,5 +187,35 @@ export default {
   display: inline-flex;
   flex-direction: column;
   gap: 10px;
+}
+.c2d-block {
+  width: 100%;
+  display: inline-flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 0px;
+  min-height: 35px;
+}
+.drop-tools {
+  padding: 3px 5px;
+  border-radius: 5px;
+  width: 28px;
+  height: 28px;
+  background-color: white;
+  overflow: hidden;
+}
+.c2d-block:hover {
+  .drop-tools {
+    border: 1px solid #EBEEF5;
+    background-color: #F2F6FC;
+  }
+}
+.content {
+  width: 100%;
+  padding: 0 10px;
+}
+.move {
+  cursor: move;
+  flex-shrink: 0;
 }
 </style>
