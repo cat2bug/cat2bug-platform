d7e933c178cc75e1273e9f5616eb6f8521984ded
yuzhantao <yuzhantao@qq.com>
2025-07-30 15:42:00 +0800

    清除默认测试页面时打开前端编译分析页面

--- cat2bug-platform-ui/vue.config.js
+++ cat2bug-platform-ui/vue.config.js
@@ -96,7 +96,7 @@ module.exports = {
         reportFilename: 'report.html',
         defaultSizes: 'gzip',
         generateStatsFile: true, // 如果为true，则Webpack Stats JSON文件将在bundle输出目录中生成
-        openAnalyzer: true, // 默认在浏览器中自动打开报告
+        openAnalyzer: false, // 默认在浏览器中自动打开报告
         statsFilename: 'stats.json', // 如果generateStatsFile为true，将会生成Webpack Stats JSON文件的名字
         statsOptions: { source: false }
       })
