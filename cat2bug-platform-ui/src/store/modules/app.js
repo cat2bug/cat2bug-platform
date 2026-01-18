c4284a5648d689b491af3817fa0cef41bcd740b9
yuzhantao <yuzhantao@qq.com>
2026-01-18 15:43:17 +0800

    设置左侧菜单默认不打开状态

--- cat2bug-platform-ui/src/store/modules/app.js
+++ cat2bug-platform-ui/src/store/modules/app.js
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie'
 
 const state = {
   sidebar: {
-    opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
+    opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : false,
     withoutAnimation: false,
     hide: false
   },
