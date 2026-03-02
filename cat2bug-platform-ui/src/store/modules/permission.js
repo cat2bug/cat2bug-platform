0ba8571bc4d8918ef9725839dd881e1ce98a8f36
yuzhantao <yuzhantao@qq.com>
2026-03-02 14:31:40 +0800

    更新内嵌打包配置

--- cat2bug-platform-ui/src/store/modules/permission.js
+++ cat2bug-platform-ui/src/store/modules/permission.js
@@ -126,7 +126,7 @@ export function filterDynamicRoutes(routes) {
 }
 
 export const loadView = (view) => {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'embedded') {
     return (resolve) => require([`@/views/${view}`], resolve)
   } else {
     // 使用 import 实现生产环境的路由懒加载
